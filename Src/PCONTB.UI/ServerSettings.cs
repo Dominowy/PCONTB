669ab5248b59f28e8e7cda4e624a9074d1f472a0
Dominik Katana <dominik.kat@outlook.com>
2024-12-26 23:51:49 +0100

    rebuild project start and add proxy

---
+++ Src/PCONTB.UI/ServerSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PCONTB.UI
+{
+    public class ServerSettings
+    {
+        public string BaseUrl { get; set; }
+        public bool ProxyIgnoreCertificateCheck { get; set; }
+        public Dictionary<string, string> ProxyEndpoints { get; set; } = new Dictionary<string, string>();
+    }
+}
