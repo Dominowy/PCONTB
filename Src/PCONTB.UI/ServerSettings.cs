b2d105eb8795655f66d7b2c098870448773cd19d
Dominik Katana <dominik.kat@outlook.com>
2025-01-07 18:16:19 +0100

    Clean project

--- Src/PCONTB.UI/ServerSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PCONTB.UI
-{
-    public class ServerSettings
-    {
-        public string BaseUrl { get; set; }
-        public bool ProxyIgnoreCertificateCheck { get; set; }
-        public Dictionary<string, string> ProxyEndpoints { get; set; } = new Dictionary<string, string>();
-    }
-}
