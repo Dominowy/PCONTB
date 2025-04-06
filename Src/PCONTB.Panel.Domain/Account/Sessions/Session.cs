1106bee8993aaab8d5c6e0e9f7f39a899a0d0945
Dominik Katana <dominik.kat@outlook.com>
2025-04-06 16:50:20 +0200

    Add session entity

---
+++ Src/PCONTB.Panel.Domain/Account/Sessions/Session.cs
@@ -0,0 +1,13 @@
+﻿using PCONTB.Panel.Domain.Account.Users;
+using PCONTB.Panel.Domain.Common;
+
+namespace PCONTB.Panel.Domain.Account.Sessions
+{
+    public class Session : Entity
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool Revoked { get; set; }
+    }
+}
