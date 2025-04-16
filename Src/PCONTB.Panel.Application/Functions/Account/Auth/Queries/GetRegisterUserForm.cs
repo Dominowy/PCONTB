a74f298d4d6432eae045b6de134cc64585ede723
Dominik Katana <dominik.kat@outlook.com>
2025-04-16 03:24:03 +0200

    add get register form from server

---
+++ Src/PCONTB.Panel.Application/Functions/Account/Auth/Queries/GetRegisterUserForm.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using PCONTB.Panel.Application.Functions.Account.Auth.Commands;
+
+namespace PCONTB.Panel.Application.Functions.Account.Auth.Queries
+{
+    public class GetRegisterUserFormRequest : IRequest<GetRegisterUserFormResponse>
+    {
+    }
+
+    public class GetRegisterFormHandler : IRequestHandler<GetRegisterUserFormRequest, GetRegisterUserFormResponse>
+    {
+        public async Task<GetRegisterUserFormResponse> Handle(GetRegisterUserFormRequest request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new GetRegisterUserFormResponse()
+            {
+                Form = new RegisterUserRequest()
+            });
+        }
+    }
+
+    public class GetRegisterUserFormResponse
+    {
+        public RegisterUserRequest Form { get; set; }
+    }
+}
