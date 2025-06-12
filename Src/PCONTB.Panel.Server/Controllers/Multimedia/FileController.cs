b78877e99a4f5a40e6d0a19f4a31652b6592440d
Dominik Katana <dominik.kat@outlook.com>
2025-06-12 20:53:56 +0200

    Add file controller

---
+++ Src/PCONTB.Panel.Server/Controllers/Multimedia/FileController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using PCONTB.Panel.Server.Controllers.Common;
+
+namespace PCONTB.Panel.Server.Controllers.Multimedia
+{
+    public class FileController : BaseController
+    {
+        public FileController(IMediator mediator) : base(mediator)
+        {
+        }
+    }
+}
